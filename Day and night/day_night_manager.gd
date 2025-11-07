0c1245fc9a6668e902a0e830ebdfdfa194f30367
Rylee Johnston <junipear19@gmail.com>
2025-11-07 16:35:43 -0700

    days are counted and displayed based on when clock goes to 8am

--- Day and night/day_night_manager.gd
+++ Day and night/day_night_manager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+#-----------Clock variables----------------
 @export var day_color: Color = Color(1, 1, 1)
 @export var night_color: Color = Color(0.25, 0.3, 0.45)
 
@@ -8,14 +9,19 @@ var current_time: float = 8 * 60.0  # Start at 8:00 AM
 
 @export var day_start: int = 6 * 60
 @export var night_start: int = 20 * 60
+var has_triggered_new_day = false
 
 var saved_time_speed: float = 60.0
 var canvas_modulate: CanvasModulate
 var clock_label: Label
 
+#----------Day counter-----------
+@onready var day_count_label: Label
+
 func _ready() -> void:
 	canvas_modulate = $CanvasModulate
 	clock_label = $UI/TimerLabel
+	day_count_label = $UI/DayCountLabel
 
 	get_tree().connect("current_scene_changed", Callable(self, "_on_scene_changed"))
 	_on_scene_changed()
@@ -32,7 +38,23 @@ func _process(delta: float) -> void:
 
 	canvas_modulate.color = canvas_modulate.color.lerp(target_color, delta * 1.5)
 	clock_label.text = _format_time(current_time)
+	
+	# counting up days
+	var new_day_time = 8 * 60
+	if current_time >= new_day_time and not has_triggered_new_day:
+		_on_new_day()
+		has_triggered_new_day = true
+	if current_time < new_day_time:
+		has_triggered_new_day = false
+	
+	var stats = get_node("/root/GlobalStats")
+	day_count_label.text = "Day: " + str(stats.day_number)
 
+func _on_new_day():
+	var stats = get_node("/root/GlobalStats")
+	stats.day_number += 1
+	print("Day:", stats.day_number)
+	
 func _format_time(minutes: float) -> String:
 	var hour = int(minutes / 60) % 24
 	var minute = int(minutes) % 60
@@ -48,11 +70,9 @@ func start_new_day() -> void:
 func freeze_time() -> void:
 	saved_time_speed = time_speed
 	time_speed = 0
-	$UI.visible = false
 
 func resume_time() -> void:
 	time_speed = saved_time_speed
-	$UI.visible = true
 
 func _on_scene_changed() -> void:
 	var scene = get_tree().current_scene
