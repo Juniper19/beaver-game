ed34c19572cb68ffd6a219724928bb509b329f4f
MicahHiggins <111819983+MicahHiggins@users.noreply.github.com>
2025-11-12 15:06:49 -0700

    Now Door and Quota Board use interaction_area

--- Doors/door.gd
+++ Doors/door.gd
@@ -1,26 +1,26 @@
-extends Area2D
+extends Node2D
 #@export var target_scene:String, FILE, ".tscn,*.scn"
 @export_file("*.tscn", ".scn") var target_scene: String
 
 func _process(delta: float) -> void:
 	var scene = get_tree().current_scene
 	if scene.is_in_group("dam"):
-		if get_overlapping_bodies().size() > 0:
-			$GoOutsideLabel.visible = true
+		if $InteractionArea.get_overlapping_bodies().size() > 0:
+			%GoOutsideLabel.visible = true
 		else:
-			$GoOutsideLabel.visible = false
+			%GoOutsideLabel.visible = false
 	elif scene.is_in_group("world"):
-		if get_overlapping_bodies().size() > 0:
-			$SleepLabel.visible = true
+		if $InteractionArea.get_overlapping_bodies().size() > 0:
+			%SleepLabel.visible = true
 		else:
-			$SleepLabel.visible = false
+			%SleepLabel.visible = false
 	
 func _input(event):
 	if event.is_action_pressed("ui_accept"): #if enter key is pressed
 		if !target_scene: #is null
 			print("no scene in this door")
 			return
-		if get_overlapping_bodies().size() > 0:
+		if $InteractionArea.get_overlapping_bodies().size() > 0:
 			next_level()
 	
 func next_level():
